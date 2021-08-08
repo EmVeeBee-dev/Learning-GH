d1b95146cec74101e6cb51d1edbdbc78dc8e29bf
Maarten van Borrendam <emveebee@xs4all.nl>
2021-08-08 20:26:26 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Maarten van Borrendam on 08/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
